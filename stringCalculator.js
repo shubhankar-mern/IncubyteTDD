8865794489d296a6189c46e4dda3ae9394be75a4
shubhankar-mern <shubhankars361@gmail.com>
2025-04-05 14:15:38 +0530

    string length more than 1 seperated by comma and nextline delimiter > passing

--- stringCalculator.js
+++ stringCalculator.js
@@ -5,11 +5,16 @@ function add(numbers){
     if(numbers.length === 1){
         return parseInt(numbers);
     }
-    if(numbers.includes(',')){
+    if(numbers.includes(',') && !numbers.includes('\n')){
         let nums = numbers.split(',');
         return nums.reduce((sum, number) => sum + parseInt(number), 0);
     }
- 
+    if(numbers.includes('\n') && numbers.includes(',')){
+        let nums = numbers.replaceAll('\n',',');
+        let nums2 = nums.split(',');
+        return nums2.reduce((sum, number) => sum + parseInt(number), 0);
+    }
+    
   
 }
 

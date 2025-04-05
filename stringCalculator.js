e1e3d23bdbcd1691dda9bcd5db87b04e4ed372b6
shubhankar-mern <shubhankars361@gmail.com>
2025-04-05 14:51:37 +0530

    string length more than 1 seperated by comma and nextLine delimiter and negative numbers and custom delimiter  > passing

--- stringCalculator.js
+++ stringCalculator.js
@@ -5,6 +5,24 @@ function add(numbers){
     if(numbers.length === 1){
         return parseInt(numbers);
     }
+    if(numbers.includes('\n') && numbers.includes('//')){
+        let idx = numbers.indexOf('//');
+        let delimiter = numbers[idx+2];
+        numbers = numbers.replaceAll('/','');
+        numbers = numbers.replaceAll('\n','');
+        let nums = numbers.split(delimiter);
+        nums = nums.filter(n => n);
+        if(nums.some(num => num.includes('-'))){
+            let s = ''
+            for(let i = 0; i < nums.length; i++){
+                if(nums[i].includes('-')){
+                    s += nums[i]+',';
+                }
+            }
+            throw new Error(`negative numbers not allowed ${s}`);
+        }
+        return nums.reduce((sum, number) => sum + parseInt(number), 0);
+    }
     if(numbers.includes(',') && !numbers.includes('\n')){
         
         let nums = numbers.split(',');
